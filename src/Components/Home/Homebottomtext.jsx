9b8c01d84ce71bd3f56fa2802a7db9f31ce6b7f7
Shubham Nagpal <shubhamnagpal018@gmail.com>
2026-04-21 22:26:45 +0530

    updated home page and created scroll transitions

--- src/Components/Home/Homebottomtext.jsx
+++ src/Components/Home/Homebottomtext.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const homebottomtext = () => {
   return (
     <div className='font-font[2] flex items-center justify-center gap-2 text-#2c1b12'>
-      <Link className='text-[3vw]  border-4 hover:border-green-700 hover:text-green-500 border-amber-200 rounded-full px-5 pt-3 uppercase' to='/projects'>Catalog</Link>
-      <Link className='text-[3vw]  border-4 hover:border-green-700 hover:text-green-500 border-amber-200 rounded-full px-5 pt-3 uppercase' to='/agence'>Profiles</Link>
+      <Link className='text-[3vw]  border-4 hover:border-green-700 hover:text-green-500 border-white rounded-full px-5 pt-3 uppercase' to='/projects'>Catalog</Link>
+      <Link className='text-[3vw]  border-4 hover:border-green-700 hover:text-green-500 border-white rounded-full px-5 pt-3 uppercase' to='/agence'>Profiles</Link>
 
     </div>
   )
--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -6,6 +6,9 @@ import Homebottomtext from '../Components/Home/Homebottomtext'
 const home = () => {
   return (
     <div>
+       <div className='absolute top-7 left-0 z-50'>
+       <img src="./Pics/logo.png" className='w-24 h-24 rounded-full object-cover  ' />
+       </div>
        <div className='h-screen w-screen fixed'>
        <Video />
        </div>
