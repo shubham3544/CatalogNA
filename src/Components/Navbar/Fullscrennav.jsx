ccc5db978a403b6b06168c88ada3ab428ee1addf
Shubham Nagpal <shubhamnagpal018@gmail.com>
2026-05-07 01:09:36 +0530

    made transition of navbar

--- src/Components/Navbar/Fullscrennav.jsx
+++ src/Components/Navbar/Fullscrennav.jsx
@@ -2,27 +2,151 @@ import React from 'react'
 
 const Fullscrennav = () => {
   return (
-    <div id='fullscreennav' className='h-screen w-full absolute bg-black '>
-        <div id='all-links' className='mt-[10vw]'>
-            <div className='link border y-2 '>
-            <h1 cla>Projects</h1>
-            <div>
-                <div>
-                  <h2> FRESH ARRIVAL </h2>
-                  <img src="/Pics/Pic1.png"/>
-                  <h2> FRESH ARRIVAL </h2>
-                  <img src="/Pics/Pic2.png"/>
-                  </div>
-                <div>
-                  <h2> FRESH ARRIVAL </h2>
-                  <img src="/Pics/Pic1.png"/>
-                  <h2> FRESH ARRIVAL </h2>
-                  <img src="/Pics/Pic2.png"/>
-                 </div>
+    <div id='fullscreennav' className='h-screen w-full py-40 absolute bg-black overflow-hidden'>
+      <div>
+
+        {/* PROJECT 1 */}
+        <div className='link relative border-y border-white overflow-hidden group'>
+          <h1 className='font-[font2] text-[8vw] text-center leading-[0.8] uppercase text-white py-5 group-hover:opacity-0 transition-all duration-300'>Projects</h1>
+
+          <div className='movelink absolute top-0 left-0 flex bg-[#83ec1b] opacity-0 group-hover:opacity-100 transition-all duration-500'>
+            <div className='moveX flex items-center'>
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>FRESH ARRIVAL</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic1.png' alt='' />
+              </div>
+
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>FRESH ARRIVAL</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic2.png' alt='' />
+              </div>
             </div>
-         </div>
-     </div>
- </div>
+
+            <div className='moveX flex items-center'>
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>FRESH ARRIVAL</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic1.png' alt='' />
+              </div>
+
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>FRESH ARRIVAL</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic2.png' alt='' />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* PROJECT 2 */}
+        <div className='link relative border-b border-white overflow-hidden group'>
+          <h1 className='font-[font2] text-[8vw] text-center leading-[0.8] uppercase text-white py-5 group-hover:opacity-0 transition-all duration-300'>Branding</h1>
+
+          <div className='movelink absolute top-0 left-0 flex bg-[#83ec1b] opacity-0 group-hover:opacity-100 transition-all duration-500'>
+            <div className='moveX flex items-center'>
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>CREATIVE DESIGN</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic3.png' alt='' />
+              </div>
+
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>CREATIVE DESIGN</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic4.png' alt='' />
+              </div>
+            </div>
+
+            <div className='moveX flex items-center'>
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>CREATIVE DESIGN</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic3.png' alt='' />
+              </div>
+
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>CREATIVE DESIGN</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic4.png' alt='' />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* PROJECT 3 */}
+        <div className='link relative border-b border-white overflow-hidden group'>
+          <h1 className='font-[font2] text-[8vw] text-center leading-[0.8] uppercase text-white py-5 group-hover:opacity-0 transition-all duration-300'>Animation</h1>
+
+          <div className='movelink absolute top-0 left-0 flex bg-[#83ec1b] opacity-0 group-hover:opacity-100 transition-all duration-500'>
+            <div className='moveX flex items-center'>
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>DIGITAL ART</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic1.png' alt='' />
+              </div>
+
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>DIGITAL ART</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic2.png' alt='' />
+              </div>
+            </div>
+
+            <div className='moveX flex items-center'>
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>DIGITAL ART</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic1.png' alt='' />
+              </div>
+
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>DIGITAL ART</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic2.png' alt='' />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* PROJECT 4 */}
+        <div className='link relative border-b border-white overflow-hidden group'>
+          <h1 className='font-[font2] text-[8vw] text-center leading-[0.8] uppercase text-white py-5 group-hover:opacity-0 transition-all duration-300'>Portfolio</h1>
+
+          <div className='movelink absolute top-0 left-0 flex bg-[#83ec1b] opacity-0 group-hover:opacity-100 transition-all duration-500'>
+            <div className='moveX flex items-center'>
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>UI UX DESIGN</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic3.png' alt='' />
+              </div>
+
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>UI UX DESIGN</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic4.png' alt='' />
+              </div>
+            </div>
+
+            <div className='moveX flex items-center'>
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>UI UX DESIGN</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic3.png' alt='' />
+              </div>
+
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] uppercase'>UI UX DESIGN</h2>
+
+              <div className='h-36 w-36 rounded-full overflow-hidden shrink-0 mx-4'>
+                <img className='h-full w-full object-cover object-[center_15%] scale-90' src='/Pics/Pic4.png' alt='' />
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
